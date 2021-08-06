d178811e0eb4dee62b9b3f11c0ca52419d2d722b
Mifa43 <mifa43kotez@gmail.com>
2021-08-06 15:07:58 +0200

    added a comment block on how to use endpoint

--- django_exemple/views.py
+++ django_exemple/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import TodoList, Item
+
 # Create your views here.
+
 def index(request, name):
+    #region how to use this ?
+    #http://localhost:8001/<someName>   In order to do a search in the database, 
+    # it is necessary to manually enter some information within the docker chontainer, which is explained in models.py
+    #endregion
     l = TodoList.objects.get(name=name)
     item = l.item_set.get(id=1)
     return HttpResponse("<h1>%s</h1><br></br><h1>%s</h1>" % (l.name, str(item.text)) )
