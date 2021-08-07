8a8d75e139db25633d5088055a079f081f0c4ac7
Mifa43 <mifa43kotez@gmail.com>
2021-08-07 12:47:48 +0200

    new definitions for html with text display functions

--- django_exemple/views.py
+++ django_exemple/views.py
@@ -3,18 +3,19 @@ from django.http import HttpResponse
 from .models import TodoList, Item
 
 # Create your views here.
-def home(request):
-    pass
-def index(request, name):
+def index(request, id):
     #region how to use this ?
     #http://localhost:8001/<someName>   In order to do a search in the database, 
     # it is necessary to manually enter some information within the docker chontainer, which is explained in models.py
     #endregion
-    l = TodoList.objects.get(name=name)
-    item = l.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><br></br><h1>%s</h1>" % (l.name, str(item.text)) )
+    l = TodoList.objects.get(id=id)
+    
+    return render(request, 'main/list.html', {"l": l})
+def home(request):
+    return render(request, 'main/home.html', {})
 
-
+def create(request):
+    return render(request, 'main/create.html', {})
 # enter to docker container with: docker exec -it DjangoProject /bin/bash
 # hit command: python3 manage.py startapp django_exemple
 # problem with privileges: sudo chown -c -R $USER:$USER (folder name)
