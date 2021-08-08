5570516b5dd3ba5f5e6f9f7be3220276047c1333
Mifa43 <mifa43kotez@gmail.com>
2021-08-08 13:44:40 +0200

    usage for costume form

--- django_exemple/views.py
+++ django_exemple/views.py
@@ -6,10 +6,33 @@ from .forms import CreateNewList
 def index(response, id):
     #region how to use this ?
     #http://localhost:8001/<someName>   In order to do a search in the database, 
-    # it is necessary to manually enter some information within the docker chontainer, which is explained in models.py
+    # it is necessary to manually enter some information within the docker container, which is explained in models.py
     #endregion
     l = TodoList.objects.get(id=id)
-    
+    #region explanation
+    # costume form
+    # the goal is to automatically update the complete value in the database, 
+    # if the check box is pressed the value in the database will be saved after pressing the save button and the value in the database will be upgraded,
+    # if we enter a value in the text input field and press the add new item button the complete value is False by default and both values are added to the database
+    #endregion
+    if response.method == 'POST':
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in l.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+            if len(txt) > 2:
+                l.item_set.create(text=txt, complete=False)
+            else:
+                print("invalid actions!")
+
+
+
     return render(response, 'main/list.html', {"l": l})
 def home(response):
     return render(response, 'main/home.html', {})
