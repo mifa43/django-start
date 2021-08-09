a4491a119d8b89010f2b1a11a40fe2d01eedf787
Mifa43 <mifa43kotez@gmail.com>
2021-08-09 11:27:52 +0200

    a function has been added to ensure that the user is added to the database

--- registration/views.py
+++ registration/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm #this is a module that we call from djanga it will provide us with an already automatically created registration form
 # Create your views here.
 def register(response):
-    form = UserCreationForm()
+    if response.method == "POST":
+        form = UserCreationForm(response.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/home")
+    else:
+        form = UserCreationForm()
     return render(response, "main/register.html", {"form": form})   #calling template and rendering django form
     
