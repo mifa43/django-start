b6a4e3e87bbd0eba09b0d826725f035e96ff6e8c
Mifa43 <mifa43kotez@gmail.com>
2021-08-09 12:18:55 +0200

    calling new model form in our views

--- registration/views.py
+++ registration/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm #this is a module that we call from djanga it will provide us with an already automatically created registration form
+from .forms import RegistrationForm
 # Create your views here.
 def register(response):
     if response.method == "POST":   #we catch a post request 
-        form = UserCreationForm(response.POST)
+        form = RegistrationForm(response.POST)
         if form.is_valid(): # if form input is valid we add the user and store in the database and redirect to the home page
             form.save()
         return redirect("/home")
     else:   #showing only a blank form
-        form = UserCreationForm()
+        form = RegistrationForm()
     return render(response, "main/register.html", {"form": form})   #calling template and rendering django form
     
