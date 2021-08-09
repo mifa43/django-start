5c219d5697818b84df837c2a46b528557e5cf6da
Mifa43 <mifa43kotez@gmail.com>
2021-08-09 11:15:50 +0200

    adding new app to instaled_apps in main settings

--- django_app/settings.py
+++ django_app/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_exemple.apps.DjangoExempleConfig'
+    'django_exemple.apps.DjangoExempleConfig',
+    'registration.apps.RegistrationConfig',
 ]
 
 MIDDLEWARE = [
